cdf70f67e48ed5f22ffdc566e3cef36da44ff006
korvin <delafer2019@gmail.com>
2023-09-16 19:51:40 +0200

    local changes

---
+++ env_zsh.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+echo "use: source env.sh"
+echo "python -m pip install <package_name>"
+
+# sudo chsh -s "/bin/zsh" "$(whoami)"
+
+alias python='python3'
+
+# Replace this with the actual path to your CPython installation directory
+CPYTHON_DIR="/home/$(whoami)/apps/Colab-traiNNer/python"
+export PYTHON_PATH="$CPYTHON_DIR"
+export PYTHON="$CPYTHON_DIR/install/bin"
+export PY_PIP="$CPYTHON_DIR/install/bin"
+export PY_LIBS="$CPYTHON_DIR/install/lib;$CPYTHON_DIR/install/include/python3.11"
+export SKIP_VENV=1
+
+
+# Check if the directory exists
+if [ -d "$CPYTHON_DIR" ]; then
+    # Add CPython binary directory to PATH
+    export PATH="$CPYTHON_DIR/install/bin:$PATH"
+    
+    echo "Standalone CPython environment activated for this session."
+    echo "Python version: $(python --version)"
+else
+    echo "CPython directory not found: $CPYTHON_DIR"
+fi
